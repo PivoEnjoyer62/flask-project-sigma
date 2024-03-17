7ae5c49ac37c4908f44501828327ed44da94e47a
Anonymous <124679005+PivoEnjoyer62@users.noreply.github.com>
2024-03-17 16:21:24 +0200

    sigmasummary

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from flask import Flask, render_template, request, redirect, url_for
 from sqlmanager import SQLManager
 
 app = Flask(__name__)
-db = SQLManager("krutoj")
-db.create_tables()
 
 
 @app.route('/')
@@ -11,9 +9,32 @@ def main():
     return render_template("main.html")
 
 
-@app.route('/login')
+@app.route('/articles')
+def articles():
+    return render_template("articles.html")
+
+
+@app.route('/create-article')
+def create_articles():
+    return render_template("create_article.html")
+
+
+@app.route('/login', methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        db = SQLManager("krutoj")
+        email = request.form["email-input"]
+        password = request.form["password-input"]
+        data = db.login_selection(email, password)
+        print(data)
+        if data is None:
+            return render_template("login.html")
+        if data[0] == email and data[1] == password:
+            return redirect(url_for("main"))
+
+        return render_template("login.html")
 
 
 @app.route('/register', methods=["GET", "POST"])
@@ -21,10 +42,11 @@ def register():
     if request.method == "GET":
         return render_template("register.html")
     if request.method == "POST":
+        db = SQLManager("krutoj")
         nickname = request.form["nickname-input"]
         email = request.form["email-input"]
         password = request.form["password-input"]
-        print(f"Nickname: {nickname};\nE-mail: {email};\nPassword: {password}.\n")
+        db.register_insertion(nickname, email, password)
         return render_template("register.html")
 
 
--- sqlmanager.py
+++ sqlmanager.py
@@ -5,7 +5,7 @@ class SQLManager:
     def __init__(self, db_name):
         self.sql = sqlite3.connect(f"{db_name}.db")
 
-    def create_tables(self):
+    def create_table_users(self):
         db_cursor = self.sql.cursor()
         db_cursor.execute(f""" 
         CREATE TABLE IF NOT EXISTS Users (
@@ -15,3 +15,25 @@ class SQLManager:
             password TEXT NOT NULL
         )
         """)
+        db_cursor.close()
+        self.sql.commit()
+
+    def register_insertion(self, nickname, email, password):
+        db_cursor = self.sql.cursor()
+        db_cursor.execute(f"""
+        INSERT INTO Users(nickname, email, password) VALUES(?, ?, ?)
+        """, [nickname, email, password])
+        db_cursor.close()
+        self.sql.commit()
+
+    def login_selection(self, email, password):
+        db_cursor = self.sql.cursor()
+        db_cursor.execute(f"""
+        SELECT email, password FROM Users WHERE email=? AND password=?
+        """, [email, password])
+        data = db_cursor.fetchone()
+        db_cursor.close()
+        self.sql.commit()
+
+        return data
+
